a964e74a68f23145fa5972491d603c7ced705bac
fanmingyong <fmyhappy@gmail.com>
2018-03-03 21:04:36 +0800

    初始导入

---
+++ spms/dao/BaseDAO.cs
@@ -0,0 +1,94 @@
+﻿using spms.util;
+using System;
+using Dapper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+namespace spms.dao
+{
+    class BaseDAO<T> where T : class
+    {
+        public List<T> ListAll()
+        {
+            using (var conn = DbUtil.getConn())
+            {
+                conn.Open();
+
+                var result = conn.GetAll<T>().ToList();
+                return result;
+            }
+
+        }
+
+        public T Load(Object primaryKey)
+        {
+            using (var conn = DbUtil.getConn())
+            {
+                conn.Open();
+
+                var result = conn.Get<T>(primaryKey);
+                return result;
+            }
+
+        }
+
+        public long Insert(T t)
+        {
+            using (var conn = DbUtil.getConn())
+            {
+                conn.Open();
+
+                var result = conn.Insert<T>(t);
+                return result;
+            }
+        }
+
+        public long BatchInsert(List<T> list)
+        {
+            using (var conn = DbUtil.getConn())
+            {
+                conn.Open();
+
+                var result = conn.Insert(list);
+                    return result;
+            }
+        }
+
+        public Boolean UpdateByPrimaryKey(T t)
+        {
+            using (var conn = DbUtil.getConn())
+            {
+                conn.Open();
+
+                var result = conn.Update<T>(t);
+                return result;
+            }
+        }
+ 
+
+        public Boolean DeleteByPrimaryKey(T t)
+        {
+            using (var conn = DbUtil.getConn())
+            {
+                conn.Open();
+
+                var result = conn.Delete<T>(t);
+                return result;
+            }
+        }
+
+        public Boolean DeleteAll()
+        {
+            using (var conn = DbUtil.getConn())
+            {
+                conn.Open();
+
+                var result = conn.DeleteAll<T>();
+                return result;
+            }
+        }
+    }
+}
---
+++ spms/util/DbUtil.cs
@@ -0,0 +1,39 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spms.util
+{
+ 
+   
+        class DbUtil
+        {
+
+            private const string connstr = "server=127.0.0.1;user id=root; password=hengxingqingdao; database=breakrule; pooling=true";
+
+            public static SqlConnection getConn()
+            {
+                return new SqlConnection(connstr);
+            }
+
+            public static void close(SqlConnection conn, SqlConnection reader)
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+
+            }
+
+        }
+     
+
+}
