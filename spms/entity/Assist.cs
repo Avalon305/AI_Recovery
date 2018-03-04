1734e12dce7674e4defcd820c3b7c1ca60723ba0
zsc <vaf714@qq.com>
2018-03-04 22:28:45 +0800

    辅助类归为一个类

---
+++ spms/entity/Assist.cs
@@ -0,0 +1,49 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spms.entity
+{
+    class Assist
+    {
+        //主键 自增
+        [Key]
+        public int Pk_Gr_Id { get; set; }
+        //数据创建时间
+        public DateTime? Gmt_Create { get; set; }
+        //数据更新时间
+        public DateTime? Gmt_Modified { get; set; }
+        //分组名字
+        public string Gr_Name { get; set; }
+
+    }
+
+    class Disease
+    {
+        //主键 自增
+        [Key]
+        public int Pk_Gr_Id { get; set; }
+        //数据创建时间
+        public DateTime? Gmt_Create { get; set; }
+        //数据更新时间
+        public DateTime? Gmt_Modified { get; set; }
+        //疾病名字
+        public string Gr_Name { get; set; }
+    }
+
+    class Diagnosis
+    {
+        //主键 自增
+        [Key]
+        public int Pk_Dn_Id { get; set; }
+        //数据创建时间
+        public DateTime? Gmt_Create { get; set; }
+        //数据更新时间
+        public DateTime? Gmt_Modified { get; set; }
+        //残疾名字
+        public string Dn_Name { get; set; }
+    }
+}
