b23c3ae1c7bbe2b9e439c30f0695145a137f97cb
zeroLRY <912060248@qq.com>
2018-03-06 16:30:20 +0800

    更新了json工具

--- spms/MainWindow.xaml.cs
+++ spms/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using spms.dao;
 using spms.entity;
 using spms.server;
 using spms.service;
+using spms.util;
 using System;
 using System.Collections.Generic;
 using System.IO.Ports;
@@ -53,6 +54,44 @@ namespace spms
             entity.Setter setter = new SetterService().getSetter();
             MessageBox.Show(setter.Set_OrganizationSort.ToString()+"-");
         }
- 
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            entity.Setter setter = new entity.Setter();
+            setter.Pk_Set_Id = 1;
+            setter.Set_Language = entity.Setter.SET_LANGUAGE_CHINA;
+            setter.Set_OrganizationName = "1";
+            setter.Set_OrganizationPhone = "2";
+            setter.Set_OrganizationSort = "3";
+            setter.Set_PhotoLocation = "4";
+
+            string str = JsonTools.Obj2JSONStr<entity.Setter>(setter);
+            MessageBox.Show(str);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            entity.Setter setter = new entity.Setter();
+            setter.Pk_Set_Id = 1;
+            setter.Set_Language = entity.Setter.SET_LANGUAGE_CHINA;
+            setter.Set_OrganizationName = "1";
+            setter.Set_OrganizationPhone = "2";
+            setter.Set_OrganizationSort = "3";
+            setter.Set_PhotoLocation = "4";
+
+
+            entity.Setter setter2 = new entity.Setter();
+            setter2.Pk_Set_Id = 1;
+            setter2.Set_Language = entity.Setter.SET_LANGUAGE_CHINA;
+            setter2.Set_OrganizationName = "1";
+            setter2.Set_OrganizationPhone = "2";
+            setter2.Set_OrganizationSort = "3";
+            setter2.Set_PhotoLocation = "4";
+            List<entity.Setter> list = new List<entity.Setter>();
+            list.Add(setter);
+            list.Add(setter2);
+            string str = JsonTools.List2JSONStr<entity.Setter>(list);
+            MessageBox.Show(str);
+        }
     }
 }
