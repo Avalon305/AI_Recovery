c6b1ef24a331cc21e4956efe52ecd2bddd1301ab
lzh1508100211 <32349817+lzh1508100211@users.noreply.github.com>
2018-03-08 16:58:40 +0800

    修改路径问题

--- spms/view/MainWindow.xaml.cs
+++ spms/view/MainWindow.xaml.cs
@@ -12,9 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using spms.entity;
-namespace spms
+using spms.view.Pages;
+using spms.view.Pages.Frame;
 
+namespace spms
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
@@ -26,22 +27,44 @@ namespace spms
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
-        //登录按钮：点击
+        //登录操作
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        {   
+            //获取用户名
+            String name = this.User_Name.Text;
+            //获取密码
+            String password = this.User_Password.Password;
+            Console.WriteLine("name:" + name);
+            Console.WriteLine("password:" + password);
+            //对用户名密码进行判断
+            if(name.Equals("123") && password.Equals("123")) {
+                //用户名密码正确进入主页面
             MainPage mainpage = new MainPage();
             this.Content = mainpage;
+            }
+            else
+            {
+                //用户名密码错误进行提示
+                Name_ErrorInfo.Content = "用户名错误";
+                Password_ErrorInfo.Content = "密码错误";
+                Console.WriteLine(Name_ErrorInfo.Content);
+                Color color = Color.FromArgb(255, 255, 0, 0);
+                Name_ErrorInfo.Foreground = new SolidColorBrush(color);
+                Password_ErrorInfo.Foreground = new SolidColorBrush(color);
 
+            }
         }
-        //设置按钮：点击
+        //打开设置页面
         private void Design(object sender, RoutedEventArgs e)
         {
-            AdvancedSettings advancedSettings = new AdvancedSettings();
-            this.Content = advancedSettings;
-            //DesignPage1 designPage = new DesignPage1();
-           // this.Content = designPage;
+            DesignPage1 designPage = new DesignPage1();
+           this.Content = designPage;
         }
-
-        
+        //重置登录表单
+        private void Resetting_Button(object sender, RoutedEventArgs e)
+        {
+            User_Name.Clear();
+            User_Password.Clear();
+        }
     }
 }
