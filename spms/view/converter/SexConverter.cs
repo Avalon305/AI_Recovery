4398e9594c3ffd2ee4eca1315254e58eb4a2b732
lzh1508100211 <32349817+lzh1508100211@users.noreply.github.com>
2018-03-15 13:55:44 +0800

    年龄、性别转换器

---
+++ spms/view/converter/SexConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace spms.view.converter
+{
+    [ValueConversion(typeof(int), typeof(String))]
+    class SexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int reValue = System.Convert.ToInt32(value);
+            string sex;
+            if (reValue == 1)
+            {
+                sex = "男";
+            }
+            else
+            {
+                sex = "女";
+            }
+            return sex;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string sexString= value.ToString();
+            int sex;
+            if (sexString.Equals("男"))
+            {
+                sex = 1;
+            }
+            else
+            {
+                sex = 0;
+            }
+            return sex;
+        }
+    }
+}
