773c02bca5b2b2b19b7b9b0d7c43d5b9dfc78763
zeroLRY <912060248@qq.com>
2018-03-17 09:19:58 +0800

    增加customdata

---
+++ spms/entity/CustomData.cs
@@ -0,0 +1,30 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spms.entity
+{
+    [Table("bdl_customdata")]
+    public class CustomData
+    {
+        //主键 自增
+        [Key]
+        public int Pk_CD_Id { get; set; }
+        /// <summary>
+        /// 自定义姓名
+        /// </summary>
+        public string CD_CustomName { get; set; }
+        /// <summary>
+        /// 自定义类型
+        /// </summary>
+        public byte? CD_Type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public byte？ Is_Deleted { get; set; }
+
+}
+}
