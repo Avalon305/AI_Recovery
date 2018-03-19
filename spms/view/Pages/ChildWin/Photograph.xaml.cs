f056904b8436fab878a9a1790d45b8f9e9723b84
lzh1508100211 <1594156197@qq.com>
2018-03-19 11:53:52 +0800

    摄像页面、二级密码页面、输入征状信息的部分修改、输入训练的部分修改、输入训练结果、输入体力评价报告、病人注册、病人条件检索、训练报告、病人信息更新

---
+++ spms/view/Pages/ChildWin/Photograph.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace spms.view.Pages.ChildWin
+{
+    /// <summary>
+    /// Photograph.xaml 的交互逻辑
+    /// </summary>
+    public partial class Photograph : Window
+
+    {
+        //GWL_STYLE表示获得窗口风格
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        //SetWindowLong：更改指定窗口的属性。
+        //
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+       
+        //取消按钮，关闭此窗体
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //获得窗口句柄
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+        }
+        public Photograph()
+        {
+            InitializeComponent();
+        }
+    }
+}
