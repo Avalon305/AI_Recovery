16cc0fb767d008466b2d0dbf346badf706e08335
lzh1508100211 <1594156197@qq.com>
2018-03-19 16:49:23 +0800

    处理window_load错误

--- spms/view/Pages/ChildWin/InputManualMvaluation.xaml.cs
+++ spms/view/Pages/ChildWin/InputManualMvaluation.xaml.cs
@@ -19,6 +19,19 @@ namespace spms.view.Pages.ChildWin
     /// </summary>
     public partial class InputManualMvaluation : Window
     {
+        //去除窗体叉号
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+        }
 
         private new bool IsEnabled = true;
         private bool UnEnabled = false;
