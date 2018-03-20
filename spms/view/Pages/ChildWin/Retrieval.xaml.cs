b03420e2021eac91563a354274d7998839a94aa8
lzh1508100211 <1594156197@qq.com>
2018-03-20 23:31:09 +0800

    手机号验证和查重

--- spms/view/Pages/ChildWin/Retrieval.xaml.cs
+++ spms/view/Pages/ChildWin/Retrieval.xaml.cs
@@ -151,22 +151,39 @@ namespace spms.view.Pages.ChildWin
         {
             inputlimited.InputLimited.OnlyInputNumbers(e);
         }
-
+        //身份证号验证和查重
         private void IsIDCard(object sender, RoutedEventArgs e)
         {
             UserService userService = new UserService();
             if (!inputlimited.InputLimited.IsIDcard(IDCard.Text) && !String.IsNullOrEmpty(IDCard.Text))
             {
-                Error_Info.Content = "请输入正确的身份证号码";
-                bubble.IsOpen = true;
+                Error_Info_IDCard.Content = "请输入正确的身份证号码";
+                bubble_IDCard.IsOpen = true;
             }
             else if(userService.GetByIdCard(IDCard.Text) !=null)
             {
-                Error_Info.Content = "该身份证已注册";
-                bubble.IsOpen = true;
+                Error_Info_IDCard.Content = "该身份证已注册";
+                bubble_IDCard.IsOpen = true;
             }else
             {
-                bubble.IsOpen = false;
+                bubble_IDCard.IsOpen = false;
+            }
+        }
+        //手机号验证和查重
+        private void IsPhone(object sender, RoutedEventArgs e)
+        {
+            if(!inputlimited.InputLimited.IsHandset(phone.Text) && !String.IsNullOrEmpty(phone.Text)){
+                Error_Info_Phone.Content = "请输入正确的手机号";
+                bubble_phone.IsOpen = true;
+            }
+            else if (userService.GetByPhone(phone.Text) != null)
+            {
+                Error_Info_Phone.Content = "该手机号已注册";
+                bubble_phone.IsOpen = true;
+            }
+            else
+            {
+                bubble_phone.IsOpen = false;
             }
         }
     }
