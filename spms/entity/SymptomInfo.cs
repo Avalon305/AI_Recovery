832f65b29d49f7abf29ed0a24024a5c0337d4469
zsc <vaf714@qq.com>
2018-03-20 10:54:03 +0800

    修改症状表结构和实体类

--- spms/entity/SymptomInfo.cs
+++ spms/entity/SymptomInfo.cs
@@ -32,45 +32,32 @@ namespace spms.entity
         public string SI_CareInfo { get; set; }
         //问诊票
         public string SI_Inquiry { get; set; }
+        //训练信息id
+        public int Pk_TI_Id { get; set; }
+        //高血压（康复前）
+        public string SI_Pre_HighPressure { get; set; }
+        //低血压（康复前）
+        public string SI_Pre_LowPressure { get; set; }
+        //心率（康复前）
+        public string SI_Pre_HeartRate { get; set; }
+        //体温（康复前）
+        public string SI_Pre_AnimalHeat { get; set; }
+        //脉搏（康复前）
+        public int SI_Pre_Pulse { get; set; }
+        //高血压（康复后）
+        public string SI_Suf_HighPressure { get; set; }
+        //低血压（康复后）
+        public string SI_Suf_LowPressure { get; set; }
+        //心率（康复后）
+        public string SI_Suf_HeartRate { get; set; }
+        //体温（康复后）
+        public string SI_Suf_AnimalHeat { get; set; }
+        //脉搏（康复后）
+        public int SI_Suf_Pulse { get; set; }
 
         public override string ToString()
         {
-            return $"{nameof(Pk_SI_Id)}: {Pk_SI_Id}, {nameof(Gmt_Create)}: {Gmt_Create}, {nameof(Gmt_Modified)}: {Gmt_Modified}, {nameof(Fk_User_Id)}: {Fk_User_Id}, {nameof(SI_IsJoin)}: {SI_IsJoin}, {nameof(SI_WaterInput)}: {SI_WaterInput}, {nameof(SI_CareInfo)}: {SI_CareInfo}, {nameof(SI_Inquiry)}: {SI_Inquiry}";
-        }
-    }
-    //症状信息关联子表
-    [Table("bdl_symptominfochild")]
-    public class SymptomInfoChild
-    {
-        //状态常量，0代表之前，1代表之后
-        public static byte? STATUS_BEFORE = 0;
-        public static byte? STATUS_AFTER = 1;
-        //主键 自增   
-        [Key]
-        public int Pk_SIC_Id { get; set; }
-        //数据创建时间
-        public DateTime? Gmt_Create { get; set; }
-        //数据更新时间
-        public DateTime? Gmt_Modified { get; set; }
-        //症状信息所属的症状ID
-        public int Fk_SI_Id { get; set; }
-        //状态
-        public Byte? Status { get; set; }
-       
-        //高血压
-        public string SIC_HighPressure { get; set; }
-        //低血压
-        public string SIC_LowPressure { get; set; }
-        //心率
-        public string SIC_HeartRate { get; set; }
-        //体温
-        public string SIC_AnimalHeat { get; set; }
-        //脉搏
-        public int SIC_Pulse { get; set; }
-
-        public override string ToString()
-        {
-            return $"{nameof(Pk_SIC_Id)}: {Pk_SIC_Id}, {nameof(Gmt_Create)}: {Gmt_Create}, {nameof(Gmt_Modified)}: {Gmt_Modified}, {nameof(Fk_SI_Id)}: {Fk_SI_Id}, {nameof(Status)}: {Status}, {nameof(SIC_HighPressure)}: {SIC_HighPressure}, {nameof(SIC_LowPressure)}: {SIC_LowPressure}, {nameof(SIC_HeartRate)}: {SIC_HeartRate}, {nameof(SIC_AnimalHeat)}: {SIC_AnimalHeat}, {nameof(SIC_Pulse)}: {SIC_Pulse}";
+            return $"{nameof(Pk_SI_Id)}: {Pk_SI_Id}, {nameof(Gmt_Create)}: {Gmt_Create}, {nameof(Gmt_Modified)}: {Gmt_Modified}, {nameof(Fk_User_Id)}: {Fk_User_Id}, {nameof(SI_IsJoin)}: {SI_IsJoin}, {nameof(SI_WaterInput)}: {SI_WaterInput}, {nameof(SI_CareInfo)}: {SI_CareInfo}, {nameof(SI_Inquiry)}: {SI_Inquiry}, {nameof(Pk_TI_Id)}: {Pk_TI_Id}, {nameof(SI_Pre_HighPressure)}: {SI_Pre_HighPressure}, {nameof(SI_Pre_LowPressure)}: {SI_Pre_LowPressure}, {nameof(SI_Pre_HeartRate)}: {SI_Pre_HeartRate}, {nameof(SI_Pre_AnimalHeat)}: {SI_Pre_AnimalHeat}, {nameof(SI_Pre_Pulse)}: {SI_Pre_Pulse}, {nameof(SI_Suf_HighPressure)}: {SI_Suf_HighPressure}, {nameof(SI_Suf_LowPressure)}: {SI_Suf_LowPressure}, {nameof(SI_Suf_HeartRate)}: {SI_Suf_HeartRate}, {nameof(SI_Suf_AnimalHeat)}: {SI_Suf_AnimalHeat}, {nameof(SI_Suf_Pulse)}: {SI_Suf_Pulse}";
         }
     }
 }
