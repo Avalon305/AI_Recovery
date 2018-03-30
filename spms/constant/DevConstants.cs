2453e38c3ee11cdbac4f10408a605a4e71ede254
zsc <vaf714@qq.com>
2018-03-30 09:12:02 +0800

    修改常量类

--- spms/constant/DevConstants.cs
+++ spms/constant/DevConstants.cs
@@ -11,10 +11,18 @@ namespace spms.constant
         /// <summary>
         /// 计时器有效
         /// </summary>
-        public static byte TIMER_VALID;
+        public static byte TIMER_VALID = 0x01;
         /// <summary>
         /// 计时器无效
         /// </summary>
-        public static byte TIMER_INVALID;
+        public static byte TIMER_INVALID = 0x02;
+        /// <summary>
+        /// 正计时
+        /// </summary>
+        public static byte COUNT = 0x00;
+        /// <summary>
+        /// 倒计时
+        /// </summary>
+        public static byte COUNT_DOWN = 0x01;
     }
 }
