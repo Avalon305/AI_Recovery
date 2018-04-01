06becaef901e86e778e037c5eae745ef7d4b281f
huihui <1719120364@qq.com>
2018-04-01 14:33:21 +0800

    串口通信，写卡成功后存库

--- spms/view/Pages/ChildWin/InputTraining .xaml.cs
+++ spms/view/Pages/ChildWin/InputTraining .xaml.cs
@@ -1608,10 +1608,25 @@ namespace spms.view.Pages.ChildWin
                         {
                             //Console.WriteLine("发卡成功");
 
-                            //保存到数据库 TODO
                             //SaveTrainInfo2DB(TrainInfoStatus.Normal);
-                            MessageBox.Show("写卡成功");
+                            //MessageBox.Show("写卡成功");
 
+
+                            //保存到数据库,在接收数据方法中
+                            try
+                            {
+                                SaveTrainInfo2DB(TrainInfoStatus.Normal);
+                            }
+                            catch (Exception exception)
+                            {
+                                return;
+                            }
+
+                            MessageBox.Show("写卡成功");
+                            Dispatcher.Invoke(new Action(() =>
+                            {
+                                this.Close();
+                            }));
                         }
                         else
                         {
