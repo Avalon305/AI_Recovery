30424e47d49d509d9e70b3b4095fc4140223415e
fanmingyong <fanmingyong@topscomm.com>
2018-04-07 20:02:40 +0800

    配置文件加密工具

--- spms/App.xaml.cs
+++ spms/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using spms.dao;
+﻿using spms.bean;
+using spms.dao;
 using spms.http;
 using spms.server;
+using spms.service;
 using spms.util;
 using System;
 using System.Collections.Generic;
@@ -26,6 +28,8 @@ namespace spms
         /// <param name="e"></param>
         protected override void OnStartup(StartupEventArgs e)
         {
+            var a = ConfigUtil.GetEncrypt("test");
+            MessageBox.Show(a);
             //加载语言
             LanguageUtils.SetLanguage();
 
@@ -34,7 +38,7 @@ namespace spms
             {
                 try
                 {
-                     NettyLuncher.getInstance().Start().Wait();
+                    NettyLuncher.getInstance().Start().Wait();
 
                 }
                 catch (AggregateException ee)
@@ -46,28 +50,46 @@ namespace spms
                     }));
                 }
             });
-             th.Start();
+            th.Start();
+
+
+            //启动远程更新
+            Thread updateTh = new Thread(() =>
+            {
+                Thread.Sleep(1000 * 3);
+                Dictionary<string, string> param = new Dictionary<string, string>();
+                var setter = new SetterService().GetSetterDAO().getSetter();
+                param.Add("version", setter.Set_Version);
+                var result = HttpSender.GET("http://127.0.0.1:8080/pcms/th", param);
+                var info = JsonTools.DeserializeJsonToObject<VersionInfo>(result);
+
+                MessageBox.Show(info.downloadUrl + "  " + info.md5);
+
+            });
+
+            updateTh.Start();
 
 
 
-            
 
             //大数据线程
-            
+
             Thread bdth = new Thread(() =>
+        {
+            SetterDAO setterDao = new SetterDAO();
+            while (true)
             {
-                SetterDAO setterDao = new SetterDAO();
-                while (true) {
-                    if (setterDao.ListAll().Count==0) {
-                        continue;
-                    }
-                    BigDataOfficer bigDataOfficer = new BigDataOfficer();
-                    bigDataOfficer.Run();
-                    int heartBeatRate = (int)CommUtil.GetBigDataRate();
-                    Thread.Sleep(1000* 300);
-                    
+                if (setterDao.ListAll().Count == 0)
+                {
+                    continue;
                 }
-            });
+                BigDataOfficer bigDataOfficer = new BigDataOfficer();
+                bigDataOfficer.Run();
+                int heartBeatRate = (int)CommUtil.GetBigDataRate();
+                Thread.Sleep(1000 * 300);
+
+            }
+        });
             bdth.Start();
 
             base.OnStartup(e);
@@ -81,6 +103,6 @@ namespace spms
             System.Environment.Exit(0);
             base.OnExit(e);
         }
-       
+
     }
 }
---
+++ spms/bean/VersionInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spms.bean
+{
+    class VersionInfo
+    {
+        public string currentVersion { get; set; }
+        public string lastVersion { get; set; }
+        public string md5 { get; set; }
+        public string downloadUrl { get; set; }
+        public string logUrl { get; set; }
+        public Boolean update { get; set; }
+
+    }
+}
