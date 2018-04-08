59481105b379d67b0498050077bfdabfdd8340e0
huihui <1719120364@qq.com>
2018-04-08 14:57:35 +0800

    修改移乘方式和动作姿势，英文

--- spms/dao/app/ExcelDao.cs
+++ spms/dao/app/ExcelDao.cs
@@ -40,7 +40,7 @@ namespace spms.dao
         {
             using (var conn = DbUtil.getConn())
             {
-                const string query = "SELECT dp.gmt_create,dp.dp_groupcount,dp.dp_groupnum,dp.dp_relaxtime,dp.dp_moveway,dp.dp_memo,dp.dp_weight,ds.ds_name FROM bdl_traininfo ti, bdl_deviceprescription dp, bdl_devicesort ds WHERE ti.pk_ti_id = dp.fk_ti_id AND dp.fk_ds_id = ds.pk_ds_id AND ti.fk_user_id=@User_Id ORDER BY dp.gmt_create";
+                const string query = "SELECT dp.gmt_create,dp.dp_groupcount,dp.dp_groupnum,dp.dp_relaxtime,dp.dp_moveway,dp.dp_weight,ds.ds_name,pr.pr_evaluate FROM bdl_traininfo ti, bdl_deviceprescription dp, bdl_devicesort ds,bdl_prescriptionresult pr WHERE ti.pk_ti_id = dp.fk_ti_id AND dp.fk_ds_id = ds.pk_ds_id AND dp.pk_dp_id=pr.fk_dp_id AND ti.fk_user_id=@User_Id ORDER BY dp.gmt_create";
 
                 return conn.Query<DevicePrescriptionExcel>(query, new { User_Id = userId }).ToList();
             }
