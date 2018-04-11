5f2167c930c5901a656074449cb952e0b80dcbcf
李泽 <18363998152@163.com>
2018-04-11 20:35:24 +0800

    dao和entity of online设备

---
+++ spms/dao/app/OnlineDeviceDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using spms.entity;
+
+namespace spms.dao.app
+{
+    public class OnlineDeviceDAO : BaseDAO<OnlineDevice>
+    {
+
+    }
+}
---
+++ spms/entity/OnlineDevice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+namespace spms.entity
+{
+    /// <summary>
+    /// 在线设备实体
+    /// </summary>
+    [Table("bdl_onlinedevice")]
+    public class OnlineDevice
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int pk_od_id { get; set; }
+        /// <summary>
+        /// 终端ID
+        /// </summary>
+        public string od_clientid { get; set; }
+        /// <summary>
+        /// 终端类型-英文
+        /// </summary>
+        public string od_clientname_en { get; set; }
+        /// <summary>
+        /// 终端类型-中文
+        /// </summary>
+        public string od_clientname_ch { get; set; }
+        /// <summary>
+        /// 最后在线时间
+        /// </summary>
+        public DateTime od_gmt_modified { get; set; }
+    }
+}
