dfd875beadeb5cc707d7d38c8ebfd9e011590fbb
lzh1508100211 <1594156197@qq.com>
2018-04-12 11:21:01 +0800

    在线设备列表

---
+++ spms/view/Pages/ChildWin/OnlineDeviceDetails.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace spms.view.Pages.ChildWin
+{
+    /// <summary>
+    /// OnlineDeviceDetails.xaml 的交互逻辑
+    /// </summary>
+    public partial class OnlineDeviceDetails : Window
+    {
+        
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        public OnlineDeviceDetails()
+        {
+            InitializeComponent();
+            A test = new A();
+            test.Name = "123";
+            test.status = 1;
+            List<A> ass = new List<A>();
+            ass.Add(test);
+            terminalstatus.ItemsSource = ass;
+            //DataGrid2.DataContext = ass;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+        }
+
+        //取消按钮，关闭此窗体
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        //回车按钮
+        private void key_dowm(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Button_OK(this, null);
+                //使键盘失去焦点，解决窗口反复出现
+                Keyboard.ClearFocus();
+            }
+
+        }
+
+        private void Button_OK(object sender, RoutedEventArgs e)
+        {
+
+        }
+        class A
+        {
+            public String Name { get; set; }
+            public int status { get; set; }
+        }
+
+        private void abc(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
