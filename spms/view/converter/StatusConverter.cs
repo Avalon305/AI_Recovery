e1aac430a0e2dcd7bda7bd0eb5e19fac9d55bd6f
lzh1508100211 <1594156197@qq.com>
2018-04-12 13:35:46 +0800

    converer

---
+++ spms/view/converter/StatusConverter.cs
@@ -0,0 +1,37 @@
+﻿using spms.util;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace spms.view.converter
+{
+    class StatusConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString().Equals("正常"))
+            {
+               
+                return new SolidColorBrush(Colors.GreenYellow);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Red); 
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int revalue = (int)value;
+
+            return DateTime.Now.Year - revalue;
+        }
+    }
+}
