351e7ad545b40658556fdbb74a65ba8758aa5309
zero.lee <18363998152@163.com>
2019-04-01 16:15:48 +0800

    还原回去，增加常量注解

--- spms/entity/TrainInfo.cs
+++ spms/entity/TrainInfo.cs
@@ -38,11 +38,12 @@ namespace spms.entity
     [Table("bdl_deviceprescription")]
     public class DevicePrescription
     {
-        ////状态常量   0没做  1 保存   2做完了   3废弃了
-        //public static byte? UNDO = 0;
-        //public static byte? SAVED = 1;
-        //public static byte? DOWN = 2;
-        //public static byte? DISCARD = 3;
+      
+        
+        //状态常量   1完成  0 未完成
+        public static byte? UNDO = 0;
+        public static byte? DOWN = 1;
+        
         //主键 自增
         [Key]
         public int Pk_DP_Id { get; set; }
