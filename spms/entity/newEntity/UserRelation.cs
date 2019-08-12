2c931112a006f9a6f634e909d9969e1297e19e9f
chenqizhao <305865088cqz@gmail.com>
2019-08-12 22:14:08 +0800

    修改实体类

--- spms/entity/newEntity/UserRelation.cs
+++ spms/entity/newEntity/UserRelation.cs
@@ -18,11 +18,13 @@ namespace spms.entity.newEntity
         [Key]
         public int Id { get; set; }
         // 关联的用户ID
-        public int Dk_user_id { get; set; }
+        public int Fk_user_id { get; set; }
         // 用户绑定手环id
         public string Bind_id { get; set; }
         // 肌力测试值
-        public string muscle_test_val { get; set; }
+        public string Muscle_test_val { get; set; }
+        // 肌力测试值创建时间
+        public DateTime? Mtv_create_time { get; set; }
         // 创建时间
         public DateTime? Gmt_create { get; set; }
         // 修改时间
--- spms/entity/newEntity/deviceprescription.cs
+++ spms/entity/newEntity/deviceprescription.cs
@@ -20,39 +20,39 @@ namespace spms.entity.newEntity
 
         // 主键 自增
         [Key]
-        public int Pk_dp_id { get; set; }
+        public long Pk_dp_id { get; set; }
         // 关联的单一训练信息ID
-        public int Fk_ti_id { get; set; }
+        public long Fk_ti_id { get; set; }
         // 关联的单一设备类型ID
-        public int Fk_ds_id { get; set; }
+        public long Fk_ds_id { get; set; }
         // 用户运动模式 0：计数模式，1：计时模式
-        public int Sport_mode { get; set; }
+        public int? Sport_mode { get; set; }
         // 设备训练模式 0康复模式，1主被动模式,2被动模式
-        public int Device_mode { get; set; }
+        public int? Device_mode { get; set; }
         // 完成状态 1完成,0未完成
-        public Byte? Dp_status { get; set; }
+        public byte? Dp_status { get; set; }
         // 医生指示建议
         public string Dp_memo { get; set; }
         // 移乘方式 0自已，1照看，2完全失能
-        public int Dp_moveway { get; set; }
+        public int? Dp_moveway { get; set; }
         // 目标运动时间
-        public int Dp_timecount { get; set; }
+        public int? Dp_timecount { get; set; }
         // 目标运动个数
-        public int Dp_target_num { get; set; }
-        // 座位高度
-        public int? Seat_height { get; set; }
-        // 靠背距离
-        public int? Backrest_distance { get; set; }
-        // 踏板位置
-        public int? Footboard_distance { get; set; }
-        // 杠杆角度
-        public double? Lever_angle { get; set; }
-        // 额外属性
-        public string Extra_setting { get; set; }
-        // 前方限制
-        public int? Front_limit { get; set; }
-        // 后方限制
-        public int? Back_limit { get; set; }
+        public int? Dp_target_num { get; set; }
+        //// 座位高度
+        //public int? Seat_height { get; set; }
+        //// 靠背距离
+        //public int? Backrest_distance { get; set; }
+        //// 踏板位置
+        //public int? Footboard_distance { get; set; }
+        //// 杠杆角度
+        //public double? Lever_angle { get; set; }
+        //// 额外属性
+        //public string Extra_setting { get; set; }
+        //// 前方限制
+        //public int? Front_limit { get; set; }
+        //// 后方限制
+        //public int? Back_limit { get; set; }
         // 顺向力
         public double? Consequent_force { get; set; }
         // 反向力
